68a5f6ca7083cace4f27d02211c77f5df2758edf
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2025-02-03 11:29:43 +0100

    add jwt and session for users

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 
 
@@ -11,6 +12,9 @@ export default async function Home({ searchParams }: {
 
     const query = (await searchParams).query
     const params = { search: query || null}
+
+    const session = await auth()
+
     const { data: posts} = await sanityFetch({query: STARTUPS_QUERY, params})
 
     return (
